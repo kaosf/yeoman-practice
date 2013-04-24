99ae62a30acca453c62155a23e3fe6620b456cbe
ka <ka.kaosf@gmail.com>
2013-04-24 17:06:37 +0900

    build first webapp

--- init.sh
+++ init.sh
@@ -12,3 +12,17 @@ npm install -g grunt-cli@0.1.6
 
 # install bower
 npm install -g bower@0.8.6
+
+mkdir my-app
+cd my-app
+
+yo webapp
+# (report?) n
+# (h5bp, bootstrap?) Y
+# (requireJS?) n
+
+grunt test #=> OK
+
+grunt server #=> OK
+
+grunt #=> OK
